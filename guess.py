96308bad66eccd5cea3f1deeaa98c993499bcc1c
Anadi <anadisingh3217@gmail.com>
2026-06-06 21:01:34 +0530

    adv

--- guess.py
+++ guess.py
@@ -4,11 +4,12 @@ while True:
 
     secret_number = random.randint(1, 10)
     attempts = 0
+    max_attempts = 5
 
     print("\n🎮 Welcome to the Guessing Game!")
     print("Guess a number between 1 and 10")
 
-    while True:  # Guessing loop
+    while attempts < max_attempts:  # Guessing loop
 
         guess = int(input("Enter your guess: "))
         attempts += 1
@@ -22,6 +23,11 @@ while True:
 
         else:
             print("📈 Too high!")
+
+        print(f"Attempts left: {max_attempts - attempts}")
+
+    if attempts == max_attempts and guess != secret_number:
+        print(f"😞 Game Over! The correct number was {secret_number}.")
 
     play_again = input("Play again? (y/n): ")
 
